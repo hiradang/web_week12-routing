e6c15b6279e16c569691ab54b0e44de84ada20c3
hiradang <sunnybinhdang@gmail.com>
2021-12-01 15:30:23 +0700

    routing

---
+++ README.md
@@ -0,0 +1,11 @@
+1. Cài đặt các module cần thiết
+    #npm i
+2. Khởi chạy server
+    #npm start
+3. Truy cập vào các file frontend theo các đường dẫn
+    /get.html
+    /update.html
+    /list.html
+Các file này được lưu trong folder views và đã được chuyển thành chế độ public
+
+Các thức hoạt động:
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path')
+const route = require('./routes');
+
+app.use(express.json());
+
+app.use('/', express.static(path.join(__dirname, 'views')))
+route(app);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
